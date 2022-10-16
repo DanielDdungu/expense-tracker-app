c6b752efb07d1c38e938e4bc75a65a85bb791341
DanielDdungu <daddungu@outlook.com>
2022-10-16 13:28:28 +0100

    .

--- src/Components/ExpenseDate.jsx
+++ src/Components/ExpenseDate.jsx
@@ -9,6 +9,8 @@ function ExpenseDate(props) {
                 <div className='expense-date__month'>{month}</div> 
                 <div className='expense-date__day'>{day}</div> 
                 <div className='expense-date__year'>{year}</div> 
+                <div className='expense-date__year'>{year}</div> 
+        </Card>
         </Card>
     )
 }
