6b4033199deb0c0387c0ed9cd74b03c98e91c359
DanielDdungu <daddungu@outlook.com>
2022-10-18 18:32:59 +0100

    Added Item Event Listener

--- src/Components/ExpenseItem.jsx
+++ src/Components/ExpenseItem.jsx
@@ -3,12 +3,20 @@ import './ExpenseItem.css'
 import ExpenseDate from'./ExpenseDate'; 
 import Card from './Card';
 function ExpenseItem(props) {  
+
+    //Button clicked Event Listener
+    let title = props.title;
+
+    const buttonClickedHandler = () => {
+       title = 'Updated';
+    }
     return (
         <Card className='expense-item'> 
             <ExpenseDate date={props.date}/> 
              <div className='expense-item__description'> 
-                <h2 className='expense-item h2' >{props.title}</h2>
+                <h2 className='expense-item h2' >{ title}</h2>
                 <div className='expense-item__price'>{props.amount} €</div>
+                <button onClick={buttonClickedHandler}>Change Tittle</button>
              </div>
         </Card>
     );
