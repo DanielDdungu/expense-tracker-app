4688d268d5a30b75a248e0be4c7ea58306827349
DanielDdungu <daddungu@outlook.com>
2022-12-06 13:07:09 +0100

    Added a dispalay form button

--- src/Components/NewExpenses/ExpenseForm.jsx
+++ src/Components/NewExpenses/ExpenseForm.jsx
@@ -80,6 +80,9 @@ const ExpenseForm = (props) => {
         
          </div>
          <div className='new-expense__actions'>
+        <button type="button" onClick={props.onCancel}>
+            Cancel
+        </button>
                     <button type='submit' >Add New Expense</button>
             </div>
     </form>
