4e503e4216770deb2c26e37f3fa7aee0be6b6792
DanielDdungu <daddungu@outlook.com>
2022-12-06 12:23:49 +0100

    Updated after prettier configurations

--- src/Components/ExpenseDate.jsx
+++ src/Components/ExpenseDate.jsx
@@ -1,17 +1,16 @@
-import './ExpenseDate.css'
-import Card from './Card';
+import "./ExpenseDate.css";
+import Card from "./Card";
 function ExpenseDate(props) {
-    const month = props.date.toLocaleString('es-ES', {month: 'long'});
-    const day = props.date.toLocaleString('es-ES', {day: '2-digit'});
-    const year = props.date.getFullYear();
-    return (
-        <Card className='expense-date'>
-                <div className='expense-date__month'>{month}</div> 
-                <div className='expense-date__day'>{day}</div> 
-                <div className='expense-date__year'>{year}</div> 
-        </Card>
-
-    )
+  const month = props.date.toLocaleString("es-ES", { month: "long" });
+  const day = props.date.toLocaleString("es-ES", { day: "2-digit" });
+  const year = props.date.getFullYear();
+  return (
+    <Card className="expense-date">
+      <div className="expense-date__month">{month}</div>
+      <div className="expense-date__day">{day}</div>
+      <div className="expense-date__year">{year}</div>
+    </Card>
+  );
 }
 
 export default ExpenseDate;
